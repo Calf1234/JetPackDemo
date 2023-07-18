ca2994be8abbd4d1d615fbb6c582a963b02b543d
我真的是黄海华 <>
2023-07-18 22:10:11 +0800

    project1 over

--- project1/src/main/java/com/apw/project1/MainActivity.kt
+++ project1/src/main/java/com/apw/project1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.apw.project1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.apw.project1.databinding.ProjectMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val dataBinding =
             DataBindingUtil.setContentView<ProjectMainBinding>(this, R.layout.project_main)
+
+        val viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        ).get(MainViewModel::class.java)
+        dataBinding?.vm = viewModel
+
+        dataBinding!!.lifecycleOwner = this
     }
+
+//    override fun onRetainCustomNonConfigurationInstance(): Any? {
+//        return super.onRetainCustomNonConfigurationInstance()
+//    }
 }
--- project1/src/main/java/com/apw/project1/MainViewModel.kt
+++ project1/src/main/java/com/apw/project1/MainViewModel.kt
@@ -1,6 +1,49 @@
 package com.apw.project1
 
+import android.app.Application
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    val phoneInfo by lazy { MutableLiveData<String>() }
+
+    init {
+        // 初始化默认值
+        phoneInfo.value = ""
+    }
+
+    var context: Context = application
+
+    // 输入
+    fun appendNumber(number: String) {
+        phoneInfo.value = phoneInfo.value + number
+    }
+
+    // 删除
+    fun backspaceNumber() {
+        // ?: 空追加
+        var length = phoneInfo.value?.length ?: 0
+
+        if (length > 0) {
+            // !! 保证一定能够拿到length
+            phoneInfo.value = phoneInfo.value?.substring(0, phoneInfo.value?.length!! - 1)
+        }
+    }
+
+    fun clear() {
+        phoneInfo.value = ""
+    }
+
+    fun callPhone() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_DIAL
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        intent.data = Uri.parse("tel:" + phoneInfo.value)
+        context.startActivity(intent)
+    }
 }
