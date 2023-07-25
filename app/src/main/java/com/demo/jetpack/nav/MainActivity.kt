78de4848ebbb2ac7ce72022b491391773460f306
我真的是黄海华 <>
2023-07-25 18:05:16 +0800

    navigation

---
+++ app/src/main/java/com/demo/jetpack/nav/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.demo.jetpack.nav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.demo.jetpack.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main7)
+    }
+}
